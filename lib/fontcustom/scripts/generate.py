f96ba49326c240776c74d883d531fec8f2818969
Justin Morris <justin@pixelbloom.com>
2013-05-14 14:57:29 +1000

    Update sizing (and use spaces for indentation - tabs? really guys, tabs? wtf?)

--- lib/fontcustom/scripts/generate.py
+++ lib/fontcustom/scripts/generate.py
@@ -6,29 +6,29 @@ import tempfile
 import json
 
 try:
-	import argparse
-	parser = argparse.ArgumentParser(description='Convert a directory of svg and eps files into a unified font file.')
-	parser.add_argument('dir', metavar='directory', type=unicode, nargs=2, help='directory of vector files')
-	parser.add_argument('--name', metavar='fontname', type=unicode, nargs='?', default='fontcustom', help='reference name of the font (no spaces)')
-	parser.add_argument('--nohash', '-n', action='store_true', help='disable hash fingerprinting of font files')
-	parser.add_argument('--debug', '-d', action='store_true', help='display debug messages')
-	args = parser.parse_args()
-	indir = args.dir[0]
-	outdir = args.dir[1]
+    import argparse
+    parser = argparse.ArgumentParser(description='Convert a directory of svg and eps files into a unified font file.')
+    parser.add_argument('dir', metavar='directory', type=unicode, nargs=2, help='directory of vector files')
+    parser.add_argument('--name', metavar='fontname', type=unicode, nargs='?', default='fontcustom', help='reference name of the font (no spaces)')
+    parser.add_argument('--nohash', '-n', action='store_true', help='disable hash fingerprinting of font files')
+    parser.add_argument('--debug', '-d', action='store_true', help='display debug messages')
+    args = parser.parse_args()
+    indir = args.dir[0]
+    outdir = args.dir[1]
 except ImportError:
-	# Older Pythons don't have argparse, so we use optparse instead
-	import optparse
-	parser = optparse.OptionParser(description='Convert a directory of svg and eps files into a unified font file.')
-	parser.add_option('--name', metavar='fontname', type='string', nargs='?', default='fontcustom', help='reference name of the font (no spaces)')
-	parser.add_option('--nohash', '-n', action='store_true', help='disable hash fingerprinting of font files')
-	parser.add_argument('--debug', '-d', action='store_true', help='display debug messages')
-	(args, posargs) = parser.parse_args()
-	indir = posargs[0]
-	outdir = posargs[1]
+    # Older Pythons don't have argparse, so we use optparse instead
+    import optparse
+    parser = optparse.OptionParser(description='Convert a directory of svg and eps files into a unified font file.')
+    parser.add_option('--name', metavar='fontname', type='string', nargs='?', default='fontcustom', help='reference name of the font (no spaces)')
+    parser.add_option('--nohash', '-n', action='store_true', help='disable hash fingerprinting of font files')
+    parser.add_argument('--debug', '-d', action='store_true', help='display debug messages')
+    (args, posargs) = parser.parse_args()
+    indir = posargs[0]
+    outdir = posargs[1]
 
 f = fontforge.font()
 f.encoding = 'UnicodeFull'
-f.design_size = 16
+f.design_size = 22
 f.em = 512
 f.ascent = 448
 f.descent = 64
@@ -40,55 +40,55 @@ files = []
 KERNING = 15
 
 for dirname, dirnames, filenames in os.walk(indir):
-	for filename in filenames:
-		name, ext = os.path.splitext(filename)
-		filePath = os.path.join(dirname, filename)
-		size = os.path.getsize(filePath)
+    for filename in filenames:
+        name, ext = os.path.splitext(filename)
+        filePath = os.path.join(dirname, filename)
+        size = os.path.getsize(filePath)
 
-		if ext in ['.svg', '.eps']:
-			if ext in ['.svg']:
-				# hack removal of <switch> </switch> tags
-				svgfile = open(filePath, 'r+')
-				tmpsvgfile = tempfile.NamedTemporaryFile(suffix=ext, delete=False)
-				svgtext = svgfile.read()
-				svgfile.seek(0)
+        if ext in ['.svg', '.eps']:
+            if ext in ['.svg']:
+                # hack removal of <switch> </switch> tags
+                svgfile = open(filePath, 'r+')
+                tmpsvgfile = tempfile.NamedTemporaryFile(suffix=ext, delete=False)
+                svgtext = svgfile.read()
+                svgfile.seek(0)
 
-				# replace the <switch> </switch> tags with 'nothing'
-				svgtext = svgtext.replace('<switch>', '')
-				svgtext = svgtext.replace('</switch>', '')
-			
-				tmpsvgfile.file.write(svgtext)
+                # replace the <switch> </switch> tags with 'nothing'
+                svgtext = svgtext.replace('<switch>', '')
+                svgtext = svgtext.replace('</switch>', '')
 
-				svgfile.close()
-				tmpsvgfile.file.close()
+                tmpsvgfile.file.write(svgtext)
 
-				filePath = tmpsvgfile.name
-				# end hack
-				
-			m.update(filename + str(size) + ';')
-			glyph = f.createChar(cp)
-			glyph.importOutlines(filePath)
+                svgfile.close()
+                tmpsvgfile.file.close()
 
-			# if we created a temporary file, let's clean it up
-			if tmpsvgfile:
-				os.unlink(tmpsvgfile.name)
+                filePath = tmpsvgfile.name
+                # end hack
 
-			# glyph.left_side_bearing = KERNING
-			# glyph.right_side_bearing = KERNING
-			glyph.width = 512
+            m.update(filename + str(size) + ';')
+            glyph = f.createChar(cp)
+            glyph.importOutlines(filePath)
 
-			# possible optimization?
-			# glyph.simplify()
-			# glyph.round()
+            # if we created a temporary file, let's clean it up
+            if tmpsvgfile:
+                os.unlink(tmpsvgfile.name)
 
-			files.append(name)
-			cp += 1
+            # glyph.left_side_bearing = KERNING
+            # glyph.right_side_bearing = KERNING
+            glyph.width = 512
+
+            # possible optimization?
+            # glyph.simplify()
+            # glyph.round()
+
+            files.append(name)
+            cp += 1
 
 if args.nohash:
-	fontfile = outdir + '/' + args.name
+    fontfile = outdir + '/' + args.name
 else:
-	hashStr = m.hexdigest()
-	fontfile = outdir + '/' + args.name + '_' + hashStr
+    hashStr = m.hexdigest()
+    fontfile = outdir + '/' + args.name + '_' + hashStr
 
 f.fontname = args.name
 f.familyname = args.name
@@ -106,12 +106,12 @@ svgfile.close()
 
 scriptPath = os.path.dirname(os.path.realpath(__file__))
 try:
-	subprocess.Popen([scriptPath + '/sfnt2woff', fontfile + '.ttf'], stdout=subprocess.PIPE)
+    subprocess.Popen([scriptPath + '/sfnt2woff', fontfile + '.ttf'], stdout=subprocess.PIPE)
 except OSError:
-	# If the local version of sfnt2woff fails (i.e., on Linux), try to use the
-	# global version. This allows us to avoid forcing OS X users to compile
-	# sfnt2woff from source, simplifying install.
-	subprocess.call(['sfnt2woff', fontfile + '.ttf'])
+    # If the local version of sfnt2woff fails (i.e., on Linux), try to use the
+    # global version. This allows us to avoid forcing OS X users to compile
+    # sfnt2woff from source, simplifying install.
+    subprocess.call(['sfnt2woff', fontfile + '.ttf'])
 
 # eotlitetool.py script to generate IE7-compatible .eot fonts
 subprocess.call('python ' + scriptPath + '/eotlitetool.py ' + fontfile + '.ttf -o ' + fontfile + '.eot', shell=True)
